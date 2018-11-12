b00a77c57024ec32bbf3c35651699e2e6221fe6f
Loris <ljaoutakas8934@bths.edu>
2018-11-11 19:08:43 -0500

    a

--- src/SciFiBot.java
+++ src/SciFiBot.java
@@ -1,4 +1,3 @@
-import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -11,7 +10,7 @@ import java.util.Scanner;
  */
 public class SciFiBot
 {
-	String[] positiveResponses = {"yes", " yeah ", "ok", "okay", "alright", "affirmative", "o.k.", "o.k"};
+	String[] positiveResponses = {"yes", " yeah ", "ok", "okay", "alright", "affirmative", "o.k.", "o.k", "fine"};
 	String[] negativeResponses = {"no", "nope", "no way", "not a chance", "nah", "i decline", "negative"};
 	//emotion can alter the way our bot responds. Emotion can become more negative or positive over time.
 	// int emotion = 0;
@@ -23,9 +22,10 @@ public class SciFiBot
 	public void chatLoop(String statement)
 	{
 		String name = "";
-		int luck = 10;
+		int luck = 0;
+		int debt = 0;
+		int money = 0;
 		int quest = (int)(randomNumber(0, requests.length));
-		String goal = goals[quest];
 		Scanner in = new Scanner (System.in);
 		System.out.println (getGreeting());
 		System.out.println (askName());
@@ -34,11 +34,33 @@ public class SciFiBot
 		System.out.println("Nice to meet you " + name + "!");
 		System.out.println("It is the year 2100. A man walks up to you and says:");
 		System.out.println("My ship broke! " + requests[quest]);
-		System.out.println("Will you help me?");
 		statement = in.nextLine();
 		if (isPositive(statement))
 		{
 			System.out.println("Thanks!");
+			String goal = goals[quest];
+			luck++;
+		}
+		if (isNegative(statement))
+		{
+			System.out.println(convince(1) + "!");
+			luck--;
+			statement = in.nextLine();
+			if (isPositive(statement))
+			{
+				System.out.println("Thanks!");
+				String goal = goals[quest];
+			}
+			if (isNegative(statement))
+			{
+				System.out.println("What if " +convince(2) + "?");
+				statement = in.nextLine();
+				if (isPositive(statement))
+				{
+					System.out.println("Thanks!");
+				}
+			}
+
 		}
 	}
 	/**
@@ -78,6 +100,11 @@ public class SciFiBot
 		return "Why do you want to " + restOfStatement + "?";
 	}
 
+	private String convince (int i)
+	{
+		String [] offers = {"100", "150", "200", "300"};
+		return "I'll give you " + offers [i] + " credits";
+	}
 	
 	/**
 	 * Take a statement with "I want <something>." and transform it into 
@@ -110,27 +137,23 @@ public class SciFiBot
 	 */
 	private boolean isPositive(String statement)
 	{
-		boolean agree = false;
-		for (int i = 0; i < positiveResponses.length; i++)
-		{
-			if (positiveResponses[i].equals(statement))
+		for (int i = 0; i < positiveResponses.length; i++) {
+			if (findKeyword(statement, positiveResponses[i]) != -1)
 			{
-				agree = true;
+				return true;
 			}
 		}
-		return agree;
+		return false;
 	}
 	private boolean isNegative(String statement)
 	{
-		boolean agree = false;
-		for (int i = 0; i < negativeResponses.length; i++)
-		{
-			if (negativeResponses[i].equals(statement))
+		for (int i = 0; i < negativeResponses.length; i++) {
+			if (findKeyword(statement, negativeResponses[i]) != -1)
 			{
-				agree = true;
+				return true;
 			}
 		}
-		return agree;
+		return false;
 	}
 
 	
